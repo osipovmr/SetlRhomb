5ec7863e724bb938e9a588bccea924c1c1a2839d
osipovmr <osipovmr@yandex.ru>
2023-01-20 23:36:30 +0600

    solid

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int height = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]);
+        printDiamond(height, width);
+    }
+    private static void printDiamond(int height, int width)   {
+        for (int i = -height; i <= height; i++) {
+            for (int j = -width; j <= width; j++) {
+                if (Math.abs(i) + Math.abs(j) <= height) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
