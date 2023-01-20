8424bfe7bd9f7cd45e039dfa6eace763aa65ab86
osipovmr <osipovmr@yandex.ru>
2023-01-21 00:04:29 +0600

    v.2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int height = Integer.parseInt(args[0]);
-        int width = Integer.parseInt(args[1]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter height: ");
+        int height = scanner.nextInt();
+        System.out.println("Enter width: ");
+        int width = scanner.nextInt();
         printDiamond(height, width);
     }
 
     private static void printDiamond(int height, int width) {
-        for (int i = -height; i <= height; i++) {
+        for (int i = -height/2; i <= height/2; i++) {
             for (int j = -width/2; j <= width/2; j++) {
                 if (Math.abs(i) + Math.abs(j) == height/2) {
                     System.out.print("#");
